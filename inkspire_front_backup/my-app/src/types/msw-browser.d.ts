03e7d1fdb41a43bc0ba08933c7b7d771117c1f60
Vivianoe <vliang361@gmail.com>
2025-12-19 20:12:22 -0500

    Remove frontend_backup

--- inkspire_front_backup/my-app/src/types/msw-browser.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'msw/browser' {
-  // Minimal ambient types to satisfy TS in this project
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function setupWorker(...handlers: any[]): any;
-}
-
-
--- inkspire_front_backup/my-app/src/types/msw-browser.ts
+++
@@ -1,7 +0,0 @@
-// Ambient module declaration to satisfy TypeScript include settings
-declare module 'msw/browser' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function setupWorker(...handlers: any[]): any;
-}
-
-
